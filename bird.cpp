5ed3eb13e4e31b2fb250077f926cad3b1f5b3b09
becky5432 <becky5432@naver.com>
2025-10-28 14:08:28 +0900

    과제 제출

--- bird.cpp
+++ bird.cpp
@@ -1,3 +1,42 @@
 #include <iostream>
 #include "bird.h"
+using namespace std;
 
+class Duck : public Bird {
+public: 
+    Duck(): Bird("Duck"){cry();}
+    void cry() {
+        cout << "Quack!" <<endl;
+    }
+    ~Duck() {
+        cry();
+    }
+};
+
+class Penguin : public Bird {
+public:
+    Penguin() :Bird("Penguin") {cry();}
+    void cry() {
+        cout << "Squawk!" <<endl;
+    }
+    void fly() {
+        cout << "This bird can't fly." <<endl;
+    }
+    ~Penguin() {
+        cry();
+    }
+};
+
+class Eagle : public Bird {
+public:
+    Eagle() :Bird("Eagle") {cry();}
+    void cry() {
+        cout << "Screech!" <<endl;
+    }
+    void swim () {
+        cout << "This bird can't swim." <<endl;
+    }
+    ~Eagle() {
+        cry();
+    }
+};
